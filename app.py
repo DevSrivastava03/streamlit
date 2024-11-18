49d760ce144eaa65bf3abc6df107ea218e54537a
DevSrivastava03 <151055979+DevSrivastava03@users.noreply.github.com>
2024-11-18 09:25:06 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
